5e02a383a134a241604a6c3a6e663548220a1412
Locatelli16 <164167466+Locatelli16@users.noreply.github.com>
2024-08-28 07:58:44 -0300

    JavaScript

---
+++ index.js
@@ -0,0 +1,30 @@
+// Usando var
+var nome = "John";
+// Usando let
+let idade = 25;
+// Usando const
+const pi = 3.14;
+//
+let a = 5;
+let b = 10
+let soma = a + b;
+console.log(soma); // 15
+//
+let a = 5;
+let b = 10
+function multiplica(x,y){
+    return x * y;
+
+}
+let produto = multiplica (a,b);
+console.log(produto); // 50
+//
+let frutas = ['Apple', 'Banana', 'Cherry'];
+console.log(frutas[1]); // Banana
+//
+let pessoa = {
+    primeironome: 'John',
+    sobrenome: 'Delon',
+    age: 25
+};
+console.log(pessoa.primeiroNome); // John
